0d7d911971562eddde07a1a436dbed6876a9316c
insider-avinash <gavandheavinash12@gmail.com>
2023-01-20 17:30:39 +0530

    adding mobile scroll app rating credstory feature

---
+++ src/common/styles/Footer/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const Footer = () => {
+  return <div className="flex absolute-center">Made with 🔥 by Avinash G</div>;
+};
+
+export default Footer;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,6 +7,10 @@ import CredExperience from "../components/credExperience";
 import CredSecurity from "../components/CredSecurity";
 import BrandLove from "../components/BrandsLove";
 import WindowPeak from "../components/WindowPeak";
+import MobileScroll from "../components/MobileScroll";
+import CredStory from "../components/CredStory";
+import AppRating from "../components/AppRating";
+import Footer from "../common/styles/Footer";
 
 const HomePage = () => {
   return (
@@ -17,11 +21,15 @@ const HomePage = () => {
       <FeelSpecial />
       <BrandLove />
       <CredExperience />
+      <MobileScroll />
       <div className="non-mobile">
         <WindowPeak />
       </div>
 
       <CredSecurity />
+      <CredStory />
+      <AppRating />
+      <Footer />
     </>
   );
 };
